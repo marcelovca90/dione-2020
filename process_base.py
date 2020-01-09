73622a8dcb3b5768f9162a5df7f1b3a78e647815
Marcelo Cysneiros <marcelovca90@gmail.com>
2020-01-09 07:06:00 -0300

    Refactoring

--- process_base.py
+++ process_base.py
@@ -5,30 +5,36 @@ from sklearn.utils import shuffle
 import numpy as np
 import json
 
-inputPath = '/home/marcelovca90/git/folder/dataset_real_vector.json'
+inputPath = '/Users/marcelocysneiros/git/dione-2020/dataset_real_vector.json'
 jsonStr = open(inputPath).read()
 data_real = json.loads(jsonStr)
 
-inputPathTest = '/home/marcelovca90/git/folder/dataset_fake_vector.json'
+inputPathTest = '/Users/marcelocysneiros/git/dione-2020/dataset_fake_vector.json'
 jsonStrTest = open(inputPathTest).read()
 data_fake = json.loads(jsonStrTest)
 
-maxWords = 1001
+p_nu_start = 0.1
+p_nu_limit = 0.0001
 
-nuu = 0.1
-while nuu > 0.0001:
+p_words_start = 10
+p_words_limit = 1001
 
-    words = 10
-    while words < maxWords:
+include_sentiment_analysis = True
+include_source_check = True
 
+p_nu = p_nu_start
+while p_nu > p_nu_limit:
+
+    p_words = p_words_start
+    while p_words < p_words_limit:
+
+        # initialize metrics arrays
         accuracy_score = []
         precision_score = []
         recall_score = []
         f1_score = []
 
-        #print(len(data_real))
-        #print(len(data_fake))
-
+        # experiment repetitions
         for seed in range(1):
 
             training_set = []
@@ -48,102 +54,75 @@ while nuu > 0.0001:
             for i in  range (len(data_fake)):
                 test_set.append(np.asarray(data_fake[i]))
                 y_true.append(-1)
-            
-            #print(len(training_set))
-            #print(len(test_set))
 
+            # choose appropriate features for the training set
             training_set_final = []
             for x in training_set:
-                #AQUI É FEITO O CONTROLE DO NUMERO DE PALAVRAS QUE ESTAO SENDO USADAS.
-                #ARMAZENAR OS "LASTS" DA ANALISE DE FONTE E DE SENTIMENTO
-                #EX: 3 ATRIBUTOS DE SENTIMENtO: 5 LAST1,LAST2,LAST3
-                #AI TIRA O NUMERO DE PALAVRAS QUE VAI USAR E JUNTA OS LASTS.
-                last1 = x[-1]
-                last2 = x[-2]
-                last3 = x[-3]
-                last4 = x[-4]
-                last5 = x[-5]
-                last6 = x[-6]
-                last7 = x[-7]
-                last8 = x[-8]
-                last9 = x[-9]
-                last10 = x[-10]
-                last11 = x[-11]
-                last12 = x[-12]
-                last13 = x[-13] # source check
+                training_temp = x[:p_words].tolist()
+                if (include_sentiment_analysis):
+                    training_temp.append(x[-1]) # sentiStrengthTitleVector | 0
+                    training_temp.append(x[-2]) # sentiStrengthTitleVector | 0
+                    training_temp.append(x[-3]) # sentiStrengthTextVector | 0
+                    training_temp.append(x[-4]) # sentiStrengthTextVector | 0
+                    training_temp.append(x[-5]) # textSubjectitivy
+                    training_temp.append(x[-6]) # textPolarity
+                    training_temp.append(x[-7]) # titleSubjectivity
+                    training_temp.append(x[-8]) # titlePolarity
+                    training_temp.append(x[-9]) # sentiWordsTitle
+                    training_temp.append(x[-10]) # sentiWordsText
+                    training_temp.append(x[-11]) # sentimentTitle
+                    training_temp.append(x[-12]) # sentimentText
+                if (include_source_check):
+                    training_temp.append(x[-13]) # sourceCheck
+                training_set_final.append(np.asarray(training_temp))
 
-                toAppendTrain = x[:words].tolist()
-                toAppendTrain.append(last1)
-                toAppendTrain.append(last2)
-                toAppendTrain.append(last3)
-                toAppendTrain.append(last4)
-                toAppendTrain.append(last5)
-                toAppendTrain.append(last6)
-                toAppendTrain.append(last7)
-                toAppendTrain.append(last8)
-                toAppendTrain.append(last9)
-                toAppendTrain.append(last10)
-                toAppendTrain.append(last11)
-                toAppendTrain.append(last12)
-                toAppendTrain.append(last13)
-
-                training_set_final.append(np.asarray(toAppendTrain))
-
-            clf = svm.OneClassSVM(nu=nuu, kernel="rbf", gamma='scale')
+            # train model
+            clf = svm.OneClassSVM(nu=p_nu, kernel="rbf", gamma='scale')
             clf.fit(training_set_final)
 
-            y_pred = []
-
+            # choose appropriate features for the test samples
             for y in test_set:
-                test = []
-                last1 = y[-1]
-                last2 = y[-2]
-                last3 = y[-3]
-                last4 = y[-4]
-                last5 = y[-5]
-                last6 = y[-6]
-                last7 = y[-7]
-                last8 = y[-8]
-                last9 = y[-9]
-                last10 = y[-10]
-                last11 = y[-11]
-                last12 = y[-12]
-                last13 = y[-13] # source check
-
-                toAppendTest = y[:words].tolist()
-                toAppendTest.append(last1)
-                toAppendTest.append(last2)
-                toAppendTest.append(last3)
-                toAppendTest.append(last4)
-                toAppendTest.append(last5)
-                toAppendTest.append(last6)
-                toAppendTest.append(last7)
-                toAppendTest.append(last8)
-                toAppendTest.append(last9)
-                toAppendTest.append(last10)
-                toAppendTest.append(last11)
-                toAppendTest.append(last12)
-                toAppendTest.append(last13)
+                test_temp = y[:p_words].tolist()
+                test_set_final = []
+                if (include_sentiment_analysis):
+                    test_temp.append(y[-1]) # sentiStrengthTitleVector | 0
+                    test_temp.append(y[-2]) # sentiStrengthTitleVector | 0
+                    test_temp.append(y[-3]) # sentiStrengthTextVector | 0
+                    test_temp.append(y[-4]) # sentiStrengthTextVector | 0
+                    test_temp.append(y[-5]) # textSubjectitivy
+                    test_temp.append(y[-6]) # textPolarity
+                    test_temp.append(y[-7]) # titleSubjectivity
+                    test_temp.append(y[-8]) # titlePolarity
+                    test_temp.append(y[-9]) # sentiWordsTitle
+                    test_temp.append(y[-10]) # sentiWordsText
+                    test_temp.append(y[-11]) # sentimentTitle
+                    test_temp.append(y[-12]) # sentimentText
+                if (include_source_check):
+                    test_temp.append(y[-13]) # sourceCheck
+                test_set_final.append(np.asarray(test_temp))
                 
-                test.append(np.asarray(toAppendTest))
+                # predict output for test sample
+                result = clf.predict(test_set_final)
                 
-                result = clf.predict(test)
-                
+                # persist prediction to array
                 if result[0] < 0:
-                    y_pred.append(-1)
-                    # print ('isso é caô.')
+                    y_pred.append(-1) # is an outlier
                 else:
-                    y_pred.append(1)
+                    y_pred.append(1) # is an inlier
 
+            # append metrics to array
             accuracy_score.append(metrics.accuracy_score(y_true, y_pred))
             precision_score.append(metrics.precision_score(y_true, y_pred))
             recall_score.append(metrics.recall_score(y_true, y_pred))
             f1_score.append(metrics.f1_score(y_true, y_pred))
         
+        # print datetime and metrics average
         dt = datetime.now()
         dt_str = '{}/{}/{} {}:{}:{}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-        print("{}\twords: {}\tnu: {}\tgamma: {}\taccuracy: {}\tprecision: {}\trecall: {}\tf1: {}".format(dt_str, words, clf.nu, clf.gamma, np.mean(accuracy_score), np.mean(precision_score), np.mean(recall_score), np.mean(f1_score)))
+        print("{}\tp_words: {}\tnu: {}\tgamma: {}\taccuracy: {}\tprecision: {}\trecall: {}\tf1: {}".format(dt_str, p_words, clf.nu, clf.gamma, np.mean(accuracy_score), np.mean(precision_score), np.mean(recall_score), np.mean(f1_score)))
 
-        words = words + 100
+        # increment number of p_words
+        p_words = p_words + 100
 
-    nuu = nuu / 10
+    # decrement nu parameter
+    p_nu = p_nu / 10
