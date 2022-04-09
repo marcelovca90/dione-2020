a8ecaa5c5ee9c14b9750aaf909d8099fa0b28af0
Marcelo Cysneiros <marcelovca90@gmail.com>
2022-04-09 12:13:02 -0300

    updates

--- process.py
+++ process.py
@@ -15,16 +15,16 @@ inputPathTest = 'dataset_fake_vector_2020_normalized.json'
 jsonStrTest = open(inputPathTest).read()
 data_fake = json.loads(jsonStrTest)
 
-p_words_start = 10
+p_words_start = 1
 p_words_limit = 1000
 
-include_sentiment_analysis = True
-include_source_check = True
-
-max_f1 = 0.0
+include_sentiment_analysis = False
+include_source_check = False
 
 plot_data_x = []
-plot_data_y = []
+plot_data_y_precision = []
+plot_data_y_recall = []
+plot_data_y_f1 = []
 
 p_words = p_words_start
 while p_words <= p_words_limit:
@@ -38,27 +38,32 @@ while p_words <= p_words_limit:
     # experiment repetitions
     for seed in range(1):
 
-        training_set = []
-        test_set = []
-        y_true = []
-        y_pred = []
+        X_train_temp = []
+        X_test_temp = []
+        Y_train_temp = []
+        Y_test_temp = []
+
+        X_train = []
+        X_test = []
+        Y_train = []
+        Y_test = []
+
         data_real = shuffle(data_real, random_state=seed)
 
         # train with real news, test with both fake and real news
         for i in range (len(data_real)):
-            if i < len(data_fake):
-            #if i < (len(data_real) - len(data_fake)):
-                training_set.append(np.asarray(data_real[i]))
+            if i < (len(data_real) - len(data_fake)):
+                X_train_temp.append(np.asarray(data_real[i]))
+                Y_train_temp.append(1) # inlier (real)
             else:
-                test_set.append(np.asarray(data_real[i]))
-                y_true.append(1) # inlier (real)
+                X_test_temp.append(np.asarray(data_real[i]))
+                Y_test_temp.append(1) # inlier (real)
         for i in range (len(data_fake)):
-            test_set.append(np.asarray(data_fake[i]))
-            y_true.append(-1) # outlier (fake)
+            X_test_temp.append(np.asarray(data_fake[i]))
+            Y_test_temp.append(-1) # outlier (fake)
 
         # choose appropriate features for the training set
-        training_set_final = []
-        for x in training_set:
+        for x in X_train_temp:
             training_temp = x[:p_words].tolist()
             if (include_sentiment_analysis):
                 training_temp.append(x[-1]) # sentiStrengthTitleVector | 0
@@ -75,64 +80,77 @@ while p_words <= p_words_limit:
                 training_temp.append(x[-12]) # sentimentText
             if (include_source_check):
                 training_temp.append(x[-13]) # sourceCheck
-            training_set_final.append(np.asarray(training_temp))
+            X_train.append(training_temp)
+        Y_train = Y_train_temp
 
         # train model
-        clf = svm.OneClassSVM(nu=0.01, kernel="rbf", gamma='scale')
-        clf.fit(training_set_final)
+        clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma='auto')
+        clf.fit(X_train)
 
         # choose appropriate features for the test samples
-        for y in test_set:
-            test_temp = y[:p_words].tolist()
-            test_set_final = []
+        for x in X_test_temp:
+            test_temp = x[:p_words].tolist()
             if (include_sentiment_analysis):
-                test_temp.append(y[-1]) # sentiStrengthTitleVector | 0
-                test_temp.append(y[-2]) # sentiStrengthTitleVector | 0
-                test_temp.append(y[-3]) # sentiStrengthTextVector | 0
-                test_temp.append(y[-4]) # sentiStrengthTextVector | 0
-                test_temp.append(y[-5]) # textSubjectitivy
-                test_temp.append(y[-6]) # textPolarity
-                test_temp.append(y[-7]) # titleSubjectivity
-                test_temp.append(y[-8]) # titlePolarity
-                test_temp.append(y[-9]) # sentiWordsTitle
-                test_temp.append(y[-10]) # sentiWordsText
-                test_temp.append(y[-11]) # sentimentTitle
-                test_temp.append(y[-12]) # sentimentText
+                test_temp.append(x[-1]) # sentiStrengthTitleVector | 0
+                test_temp.append(x[-2]) # sentiStrengthTitleVector | 0
+                test_temp.append(x[-3]) # sentiStrengthTextVector | 0
+                test_temp.append(x[-4]) # sentiStrengthTextVector | 0
+                test_temp.append(x[-5]) # textSubjectitivy
+                test_temp.append(x[-6]) # textPolarity
+                test_temp.append(x[-7]) # titleSubjectivity
+                test_temp.append(x[-8]) # titlePolarity
+                test_temp.append(x[-9]) # sentiWordsTitle
+                test_temp.append(x[-10]) # sentiWordsText
+                test_temp.append(x[-11]) # sentimentTitle
+                test_temp.append(x[-12]) # sentimentText
             if (include_source_check):
-                test_temp.append(y[-13]) # sourceCheck
-            test_set_final.append(np.asarray(test_temp))
+                test_temp.append(x[-13]) # sourceCheck
+            X_test.append(test_temp)
+        Y_test = Y_test_temp
+
+        #print('{} {} {} {}'.format(len(X_train), len(X_test), len(Y_train), len(Y_test)))
             
-            # predict output for test sample
-            result = clf.predict(test_set_final)
-            
-            # persist prediction to array
-            if result[0] < 0:
-                y_pred.append(-1) # is an outlier
-            else:
-                y_pred.append(1) # is an inlier
+        # predict and persist outputs for test samples
+        # -1 is an outlier, +1 is an inlier
+        Y_pred = clf.predict(X_test)
 
         # append metrics to array
-        accuracy_score.append(metrics.accuracy_score(y_true, y_pred))
-        precision_score.append(metrics.precision_score(y_true, y_pred))
-        recall_score.append(metrics.recall_score(y_true, y_pred))
-        f1_score.append(metrics.f1_score(y_true, y_pred))
+        accuracy_score.append(metrics.accuracy_score(Y_test, Y_pred))
+        precision_score.append(metrics.precision_score(Y_test, Y_pred))
+        recall_score.append(metrics.recall_score(Y_test, Y_pred))
+        f1_score.append(metrics.f1_score(Y_test, Y_pred))
     
     # print datetime and metrics average
     dt = datetime.now()
     dt_str = '{}/{}/{} {}:{}:{}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     print("{}\tp_words: {}\tnu: {}\tgamma: {}\taccuracy: {:.4f}\tprecision: {:.4f}\trecall: {:.4f}\tf1: {:.4f}".format(dt_str, p_words, clf.nu, clf.gamma, np.mean(accuracy_score), np.mean(precision_score), np.mean(recall_score), np.mean(f1_score)))
 
-    if np.max(f1_score) > max_f1:
-        max_f1 = np.max(f1_score)
-
     plot_data_x.append(p_words)
-    plot_data_y.append(np.mean(f1_score))    
+    plot_data_y_precision.append(np.mean(precision_score))
+    plot_data_y_recall.append(np.mean(recall_score))
+    plot_data_y_f1.append(np.mean(f1_score))
 
     # increment number of p_words
     p_words = p_words + 10
 
 # plot
-print('max f1 score = {}'.format(max_f1))
-plt.plot(plot_data_x, plot_data_y)
+fig,ax = plt.subplots()
+
+plot_data_y_precision_mean = [np.mean(plot_data_y_precision) for i in plot_data_x]
+ax.plot(plot_data_x, plot_data_y_precision, label='precision', color='darkred')
+ax.plot(plot_data_x, plot_data_y_precision_mean, label='precision (mean)', color='red', linestyle='--')
+print('precision (mean) = ', plot_data_y_precision_mean[0])
+
+plot_data_y_recall_mean = [np.mean(plot_data_y_recall) for i in plot_data_x]
+ax.plot(plot_data_x, plot_data_y_recall, label='recall', color='darkgreen')
+ax.plot(plot_data_x, plot_data_y_recall_mean, label='recall (mean)', color='green', linestyle='--')
+print('recall (mean) = ', plot_data_y_recall_mean[0])
+
+plot_data_y_f1_mean = [np.mean(plot_data_y_f1) for i in plot_data_x]
+ax.plot(plot_data_x, plot_data_y_f1, label='f1', color='darkblue')
+ax.plot(plot_data_x, plot_data_y_f1_mean, label='f1 (mean)', color='blue', linestyle='--')
+print('f1 (mean) = ', plot_data_y_f1_mean[0])
+
+legend = ax.legend(loc='best')
 plt.gca().grid()
 plt.show()
